2bfd5751d666a10bdd1c95041576e4d14daec078
majakochanowska <51491969+majakochanowska@users.noreply.github.com>
2020-03-22 20:20:46 +0100

    mobile menu

--- functions.php
+++ functions.php
@@ -7,9 +7,18 @@ add_action( 'wp_enqueue_scripts', 'furnitures_assets' );
 
 function furnitures_register_my_menus() {
     register_nav_menus(
-      array('primary' => __( 'Primary Menu' ),)
+      array(
+          'primary' => __( 'Primary Menu' ),
+          'mobile' => __( 'Mobile Menu' )
+        )
      );
    }
 add_action( 'init', 'furnitures_register_my_menus' );
 
 wp_enqueue_script('mobile-navigation', get_template_directory_uri() .'/js/mobile-navigation.js', array('jquery'), null, true);
+
+/*function furnitures_css_scripts() {
+     
+    wp_enqueue_style( 'fontawesome','https://use.fontawesome.com/fd63e88b7e.js');
+}
+add_action( 'wp_enqueue_scripts', 'furnitures_css_scripts' );*/
--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans|Pontano+Sans|Syncopate|Abel&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/b1b2166a59.js" crossorigin="anonymous"></script>
     <title><?php bloginfo( 'name' ); ?></title>
     <?php wp_head() ?>
 </head>
@@ -17,9 +18,13 @@
             <h1 id="site-title"><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
             <h3 id="site-description"><?php bloginfo( 'description' ); ?></h3>
         </div>
-        <div id="show-nav"><a href="#">Toggle</a></div>
+        <button id="show-nav"><i class="fas fa-bars"></i></button>
         <nav class="navigation-menu">
             <?php $args = [ 'theme_location' => 'primary' ]; ?>
+			<?php wp_nav_menu($args) ?>
+        </nav>
+        <nav class="mobile-menu">
+            <?php $args = [ 'theme_location' => 'mobile' ]; ?>
 			<?php wp_nav_menu($args) ?>
         </nav>
     </header>

167d4329ea7a21e5897d0ec4be851bbf3837d0e9
majakochanowska <51491969+majakochanowska@users.noreply.github.com>
2020-03-28 12:16:39 +0100

    404 page

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ */
+get_header();
+?>
+
+<article class="post">
+    <h2 style="text-align:center"><?php _e( 'Nie znaleziono strony', 'furnitures' ); ?></h2>
+    <p style="text-align:center"><?php _e( 'Ta strona nie istnieje. Sprawdź, czy podałeś/aś prawidłowy adres.', 'furnitures' ); ?></p>
+</article>
+
+<?php
+get_footer();
+?>
+
